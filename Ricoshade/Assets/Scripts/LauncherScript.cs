94443569e0eec97079b84502dff9e319b582110f
fishermanKing <55892639+fishermanKing@users.noreply.github.com>
2020-01-10 17:23:28 -0500

    Created Launcher + script

---
+++ Ricoshade/Assets/Scripts/LauncherScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherScript : MonoBehaviour
+{
+    SpringJoint spring;
+    public float pullSpeed = 0.01f;
+    void Start()
+    {
+        spring = GetComponent<SpringJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Start"))
+        {
+            print("Pull");
+            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Max(transform.localPosition.y-pullSpeed,-16), transform.localPosition.z);
+            spring.spring = 0;
+        }
+        else
+        {
+            spring.spring = 2000;
+        }
+    }
+}
