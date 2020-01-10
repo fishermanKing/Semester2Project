6d6f58b990d84e240bad0940372cfcbcfeda6eb4
fishermanKing <55892639+fishermanKing@users.noreply.github.com>
2020-01-10 14:11:10 -0500

    The actual game

---
+++ Ricoshade/Assets/Scripts/BumperScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float hitStrength = 700f;
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == ("Pinball"))
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.AddForce((other.transform.position-transform.position)*hitStrength);
+        }
+    }
+}
---
+++ Ricoshade/Assets/Scripts/FlipperScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float restPosition = 0f;
+    public float pressedPosition = 40f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+    HingeJoint hinge;
+
+    public enum Flipper { LeftFlip, RightFlip };
+    public Flipper FlipperSide;
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+        if (Input.GetButton(FlipperSide.ToString()))
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+    }
+}
