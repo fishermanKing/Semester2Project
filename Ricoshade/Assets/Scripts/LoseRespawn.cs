c93340b2234661d16ef4f41761ef9dffac44f881
SpencersFault <SpencersFault@users.noreply.github.com>
2020-01-24 16:52:07 -0500

    PinballLives

---
+++ Ricoshade/Assets/Scripts/LoseRespawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseRespawn : MonoBehaviour
+{
+    [SerializeField] private Transform pinball;
+    [SerializeField] private Transform respawn;
+    public int pinballLives = 3;
+    public GameObject playable;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        pinball.transform.position = respawn.transform.position;
+        pinballLives --;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pinballLives == 0)
+        {
+            playable.SetActive(false);
+            Debug.Log("Game Over");
+
+        }
+    }
+}
