c0417f987e8da476bafdd9639b80e84f28fc2dc6
fishermanKing <55892639+fishermanKing@users.noreply.github.com>
2020-01-27 16:43:13 -0500

    KillBox

---
+++ Ricoshade/Assets/Scripts/killScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle"){
+            other.enabled = false;
+        }
+    }
+}
