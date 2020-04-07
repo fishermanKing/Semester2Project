30923f3dc49dc87fc892725969fc97ac268273fd
Sophia Vogan <sophiavogan@gmail.com>
2020-04-07 15:39:58 -0400

    Level Transition

---
+++ Assets/transitionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject ObstacleController;
+    TextMesh LevelName;
+    string[] names = { "River Styx", "Elysium", "Mourning Fields", "Tartarus", "Erebus" };
+    int level = 0;
+
+    void Start()
+    {
+        ObstacleController = GameObject.Find("ObstacleController");
+        level = ObstacleController.GetComponent<ObstacleGeneration>().currentLevel;
+        LevelName = transform.GetComponentInChildren<TextMesh>();
+        LevelName.text = names[level];
+    }
+}
