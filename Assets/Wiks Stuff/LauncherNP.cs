70888fbcdd3b59b0efb81ebfd0ed0e3fa5335855
Comp0sator <Quarterinchspam@yahoo.com>
2020-04-09 17:07:50 -0400

    Added new Non-Physical Launcher

---
+++ Assets/Wiks Stuff/LauncherNP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherNP : MonoBehaviour
+{
+    public float power = 1000;
+    GameObject ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ball)
+        {
+            if(Input.GetKeyUp(KeyCode.Space))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(power * Vector3.forward);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pinball")
+        {
+            ball = other.gameObject;
+        }
+    }
+}
